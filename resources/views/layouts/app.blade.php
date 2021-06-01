0d6eb4d75dcb4bf3a0ffd09571cd3932b33d7892
mohamed fayez <mf429866@gmail.com>
2021-06-01 11:56:10 -0400

    finsh first part

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,25 +87,7 @@
         </main>
     </div>
 
-{{--    <script src="{{asset('js/jquery/jquery.min.js')}}"></script>--}}
-{{--    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>--}}
-    <script>
-        // $(".image").change(function () {
-        //     console.log("sss");
-        //
-        //     if (this.files && this.files[0]) {
-        //         var reader = new FileReader();
-        //
-        //         reader.onload = function (e) {
-        //             $('.image-preview').attr('src', e.target.result);
-        //         }
-        //
-        //         reader.readAsDataURL(this.files[0]);
-        //     }
-        //
-        // });
-
-    </script>
+    <script src="{{asset('js/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('js/image-preview.js')}}"></script>
 
 </body>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -92,39 +92,5 @@
         </div>
     </section>
 
-    <input type="text" id="test">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-    <script>
-        document.querySelector(".image").onchange =  function (){
-            console.log("wwwwwwww");
-            if (this.files && this.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('.image-preview').attr('src', e.target.result);
-                }
-
-                reader.readAsDataURL(this.files[0]);
-            }
-
-        }
-
-        $(".image").change(function () {
-            console.log("sss");
-
-            if (this.files && this.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('.image-preview').attr('src', e.target.result);
-                }
-
-                reader.readAsDataURL(this.files[0]);
-            }
-
-        });
-    </script>
-
 @endsection
 
